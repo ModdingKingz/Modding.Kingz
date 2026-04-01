11d922431981f223c8dd9cc4ea9dd03620639b8a
ModdingKingz <info.moddingkingz@gmail.com>
2026-04-01 23:09:56 +0200

    Add files via upload

---
+++ mods-data_v2.js
@@ -0,0 +1,17 @@
+// MODS DATENBANK — Hier alle Mods bearbeiten
+// active: true = wird angezeigt | active: false = versteckt
+// Felder: id, active, name, version, downloads, image (Pfad), link
+const MODS = [
+  { id: 1000001, active: true,  name: "P30Lite Root Method 2026", version: "0.01.0 | Payed", downloads: 0,  image: "images/android.png", link: "https://moddingkingz.github.io/M-Kz.Premium/" },
+  { id: 1000002, active: true,  name: "In Work...",               version: "0.0.0",           downloads: 4,  image: "logo.png",           link: "https://linkvertise.com/LINK-HIER" },
+  { id: 1000003, active: false, name: "Mod Name 3",               version: "1.0.0",           downloads: 0,  image: "images/mod3.jpg",    link: "https://linkvertise.com/LINK-HIER" },
+  { id: 1000004, active: false, name: "Mod Name 4",               version: "1.0.0",           downloads: 0,  image: "images/mod4.jpg",    link: "https://linkvertise.com/LINK-HIER" },
+  { id: 1000005, active: false, name: "Mod Name 5",               version: "1.0.0",           downloads: 0,  image: "images/mod5.jpg",    link: "https://linkvertise.com/LINK-HIER" },
+  { id: 1000006, active: false, name: "Mod Name 6",               version: "1.0.0",           downloads: 0,  image: "images/mod6.jpg",    link: "https://linkvertise.com/LINK-HIER" },
+  { id: 1000007, active: false, name: "Mod Name 7",               version: "1.0.0",           downloads: 0,  image: "images/mod7.jpg",    link: "https://linkvertise.com/LINK-HIER" },
+  { id: 1000008, active: false, name: "Mod Name 8",               version: "1.0.0",           downloads: 0,  image: "images/mod8.jpg",    link: "https://linkvertise.com/LINK-HIER" },
+  { id: 1000009, active: false, name: "Mod Name 9",               version: "1.0.0",           downloads: 0,  image: "images/mod9.jpg",    link: "https://linkvertise.com/LINK-HIER" },
+  { id: 1000010, active: false, name: "Mod Name 10",              version: "1.0.0",           downloads: 0,  image: "images/mod10.jpg",   link: "https://linkvertise.com/LINK-HIER" },
+  { id: 1000011, active: false, name: "Mod Name 11",              version: "1.0.0",           downloads: 0,  image: "images/mod11.jpg",   link: "https://linkvertise.com/LINK-HIER" },
+  { id: 1000012, active: false, name: "Mod Name 12",              version: "1.0.0",           downloads: 0,  image: "images/mod12.jpg",   link: "https://linkvertise.com/LINK-HIER" },
+];
